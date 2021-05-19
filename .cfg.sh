904aa82c2b18e66e7ab06abfa8358aa809a8c172
NevilleJS <nevillejs24@gmail.com>
2021-05-19 14:17:26 +0530

    removed .cfg.sh

--- .cfg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
-echo ".cfg" >> .gitignore
-git clone --bare git@github.com:NevilleJS/dotfiles.git $HOME/.cfg
-config checkout
-config config --local status.showUntrackedFiles no
