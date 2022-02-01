8cd8170f4eae8e376a1efddfb34c1adf93af6ae7
NevilleJS <nevillejs24@gmail.com>
2022-02-01 11:52:01 +0530

    hmm

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }" &
 nitrogen --restore &
 picom --experimental-backends &
 sxhkd &
